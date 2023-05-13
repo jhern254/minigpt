01386d8d67407ed6ac064a221b070f9a4d39f07a
jhern254 <jhern254@ucr.edu>
2023-05-12 18:23:30 -0700

    Add encoding, decoding, and split data functions

--- gpt_demo/model_build.py
+++ gpt_demo/model_build.py
@@ -6,9 +6,14 @@ import numpy as np
 import os
 import logging
 import requests
+import torch
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 CSV_LINK  = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+BLOCK_SIZE = 8
 
 def confirm_overwrite(filename: str) -> bool:
     """Check if the specified file already exists and prompt the user for confirmation before overwriting.
@@ -22,7 +27,6 @@ def confirm_overwrite(filename: str) -> bool:
         if overwrite.lower() != 'y':
             return False
     return True
-
 
 def download_file(url: str) -> None:
     """Download a file from the given URL.
@@ -47,7 +51,6 @@ def download_file(url: str) -> None:
     with open(filename, 'wb') as file:
         file.write(response.content)
 
-
 def read_file(filename: str) -> str:
     """Read the contents of the given file and return them as a string.
 
@@ -68,34 +71,110 @@ def read_file(filename: str) -> str:
         contents = file.read()
     return contents
 
-
 def getData() -> str:
     """Download data from link"""
     download_file(url = CSV_LINK)
     arr = read_file("./input.txt")
     return arr
 
+# TODO: following def. signature needs >= Python 3.9
+#def create_mapping(chars: str) -> tuple[dict[str, int], dict[int, str]]:
+def create_mapping(chars: str) -> tuple:
+    """
+    Create a mapping from characters to integers and integers to characters.
+
+    Args:
+    - chars (str): a string containing the characters for mapping
+
+    Returns:
+    - stoi (dict): a dictionary mapping characters to integers
+    - itos (dict): a dictionary mapping integers to characters
+    """
+    stoi = {ch: i for i, ch in enumerate(chars)}
+    itos = {i: ch for i, ch in enumerate(chars)}
+    return stoi, itos
+
+#def encode(s: str, mapping: dict[str, int]) -> list:
+def encode(s: str, mapping: dict) -> list:
+    """
+    Encode a string into a list of integers using a character-to-integer mapping.
+
+    Args:
+    - s (str): the input string to be encoded
+    - mapping (dict): a dictionary mapping characters to integers
+
+    Returns:
+    - encoded (list): a list of integers representing the encoded string
+    """
+    encoded = lambda s: [mapping[c] for c in s]
+    return encoded(s)
+
+#def decode(encoded: list[int], mapping: dict[int, str]) -> str:
+def decode(encoded: list, mapping: dict) -> str:
+    """
+    Decode a list of integers into a string using an integer-to-character mapping.
+
+    Args:
+    - encoded (list): a list of integers to be decoded
+    - mapping (dict): a dictionary mapping integers to characters
+
+    Returns:
+    - decoded (str): the decoded string
+    """
+    decoded = lambda l: ''.join([mapping[i] for i in l])
+    return decoded(encoded)
+
+# Example usage
+# chars = "abcdefghijklmnopqrstuvwxyz"
+# stoi, itos = create_mapping(chars)
+# encoded_string = encode("hii there", stoi)
+# decoded_string = decode(encoded_string, itos)
+
+def split_data(data: torch.Tensor) -> tuple:
+    """
+    Split the input data into training and validation datasets.
+
+    Args:
+    - data (torch.Tensor): Input tensor to be split
+
+    Returns:
+    - tuple[torch.Tensor, torch.Tensor]: Tuple containing the training data and validation data tensors
+    """
+    # 90/10 split
+    n = int(0.9*len(data))
+    train_data = data[:n]
+    val_data = data[n:]
+    return train_data, val_data
 
 
 
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
+    # get data from github
+    text = getData()
+#    print(type(text))
+    logging.debug("length of dataset in characters: %d", len(text))
+    logging.debug((text[:100]))
 
-    if(True):
-        text = getData()
-#        print(book[:1000])
+# NOTE: turn str text into set, then list so it's ordered, then sort
+    chars = sorted(list(set(text)))
+    vocab_size = len(chars)
+    logging.debug(''.join(chars))
+    logging.debug(vocab_size)
 
-# ask chatgpt for explanation and learn to code like karpathy
-        if(True):
-            chars = sorted(list(set(text)))
-            vocab_size = len(chars)
-            print(''.join(chars))
-            print(vocab_size)
+    # make mapping
+    stoi, itos = create_mapping(chars)
+    logging.debug(encode("hi there", stoi))
+    logging.debug(decode(encode("hi there", stoi), itos))
 
+    # encode the entire text dataset and store it into a torch.Tensor
+    data = torch.tensor(encode(text, stoi), dtype=torch.long)
+    logging.debug(data.shape, data.dtype)
+    logging.debug(data[:100])
 
-
-
+    train_data, val_data = split_data(data)
+    logging.debug(train_data[:BLOCK_SIZE+1])
 
 
 
